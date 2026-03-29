f0b7b5115d6c00f68595a8fb3fa04e085eafd9af
Javid <118032087+AstredStar@users.noreply.github.com>
2026-03-29 02:46:33 -0400

    Add js integration into page
    Add js for removing and toggling read state of cards
    TODO dialogue and card adding

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 import * as library from "./utils/library.js";
+
+const card_container = document.querySelector("#card-container");
+const card_template = document.querySelector("#card-template");
+const card_adder = document.querySelector("#add-card");
+
+function createCard(book) {
+    const card = card_template.content.cloneNode(true);
+
+    card.querySelector(".title").textContent = book.title;
+    card.querySelector(".author").textContent = book.author;
+    card.querySelector(".page-count").textContent = book.pages;
+  
+    const status = card.querySelector(".status");
+    status.classList.toggle("read", book.read);
+
+    status.onclick = (e)=>{
+        book.read = !book.read;
+        e.target.classList.toggle("read", book.read);
+    }
+
+    card.id = book.id;
+
+    card.querySelector(".close").onclick = (e)=>{
+        library.delBook(book.id);
+        displayCards();
+    }
+
+    return card;
+}
+
+function displayCards(){
+    card_container.replaceChildren();
+    for (const book of library.listBooks()){
+        const card = createCard(book);
+        card_container.appendChild(card);
+        
+    }
+    card_container.appendChild(card_adder);
+}
+
+displayCards()
+
+for (const book in library.listBooks()){
+    console.log(book);
+}
