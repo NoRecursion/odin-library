338eef1c97b445b0d02640472ef0d1bab9be1b56
Javid <118032087+AstredStar@users.noreply.github.com>
2026-04-12 15:26:41 -0400

    Refactor and make a library class

--- utils/library.js
+++ utils/library.js
@@ -1,37 +1,79 @@
-const genID = function*(){
-    let n = 0;
-    let bookID;
-    while(true){
-        n++;
-        bookID = "book-" + n;
-        yield bookID;
+export const Book = (()=>{
+    
+    let idCount = 0 
+    
+    
+    function genID(){
+        return `book-${idCount++}`
     }
-}()
-
-export function Book(title,author,pages,read){
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
+    
+    
+    function Book(title,author,pages,read){
+        if (!new.target) {
+            throw Error("You must use the 'new' operator to call the constructor");
+        }
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = genID();
     }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = genID.next().value;
-}
 
-const bookMap = new Map();
+    return Book;
+})()
 
-export function addBook(title,author,pages,read){
-    const book = new Book(title,author,pages,read);
-    bookMap.set(book.id, book);
-}
 
-export const delBook = Map.prototype.delete.bind(bookMap);
+export const Library = (()=>{
+    const p_bookMap = new WeakMap() 
 
-export const getBook = Map.prototype.get.bind(bookMap);
+    function Library(list){
+        if (!new.target) {
+            throw Error("You must use the 'new' operator to call the constructor");
+        }
 
-export const listBooks = Map.prototype.values.bind(bookMap);
+        p_bookMap.set(this, new Map());
 
-addBook("The Great Gatsby", "F. Scott Fitzgerald",240,true);
-addBook("Pride and Prejudice","Jane Austen",464, false);
-addBook("The Stranger", "Albert Camus",152, true );
+        for (const [title, author, pages, read] of list) {
+            this.addBook(title, author, pages, read);
+        }
+    }
+
+    Library.prototype.addBook = function(title, author, pages, read){
+        const book = new Book(title, author, pages, read);
+        const bookMap = p_bookMap.get(this);
+        bookMap.set(book.id, book);
+    }
+
+    Library.prototype.delBook = function(id) {
+        const bookMap = p_bookMap.get(this);
+        bookMap.delete(id);
+    }
+
+    Library.prototype.getBook = function(id) {
+        const bookMap = p_bookMap.get(this);
+        return bookMap.get(id);
+    }
+
+    Library.prototype.makeIterator = function() {
+        const bookMap = p_bookMap.get(this);
+        return bookMap.values();
+    }
+
+    Library.prototype.listBooks = function() {
+        const bookMap = p_bookMap.get(this);
+        return Array.from(bookMap.values());
+    }
+    
+    Object.defineProperty(Library, 'basicBooks', {
+        value:Object.freeze([
+            ["The Great Gatsby", "F. Scott Fitzgerald", 240, true],
+            ["Pride and Prejudice", "Jane Austen", 464, false],
+            ["The Stranger", "Albert Camus", 152, true],
+        ]),
+        writable: false,
+        configurable: false,
+        enumerable: false,
+    });
+
+    return Library;
+})()
